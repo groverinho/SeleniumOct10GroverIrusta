39ebcc986fb4af1e0d16fb7d1bb604c168c3d9a6
gIrusta <grover.irusta@coxautoinc.com>
2022-10-10 22:16:46 -0400

    Adding last steps (6 & 7)

--- src/Main.java
+++ src/Main.java
@@ -114,6 +114,41 @@ public class Main {
         Assert.assertTrue(isDisplayed, "'"+product + "' product is not available");
     }
 
+    public void removeProduct2(String product) {
+        PageUtil.subLog("Click on remove button");
+        WebElement element = PageUtil.getElement(driver, By.xpath("//button[@id='remove-sauce-labs-backpack']"));
+        Assert.assertTrue(element != null && element.isDisplayed(), "Remove Button is not displayed");
+        element.click();
+
+        PageUtil.subLog("Verify if "+product+" product was removed ? ");
+        element = PageUtil.getElement(driver, By.xpath("//div[text()='"+product+"']"));
+        boolean isDisplayed = element != null && element.isDisplayed();
+        PageUtil.subLog("Was '"+product+ "' product removed ? "+ isDisplayed);
+        Assert.assertFalse(isDisplayed, "'"+product + "' product is still displaying");
+    }
+
+    public void goToInventoryPage() {
+        PageUtil.subLog("Click on continue shopping button");
+        WebElement element = PageUtil.getElement(driver, By.xpath("//button[@id='continue-shopping']"));
+        boolean isDisplayed = element != null && element.isDisplayed();
+
+        PageUtil.subLog("Is the continue shopping button displayed ? " + isDisplayed);
+        Assert.assertTrue(isDisplayed, "Continue shopping button is not displayed");
+        element.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Inventory page is not loaded");
+    }
+
+    public void verifyProductRemoved() {
+        PageUtil.subLog("Verify if the product is selected");
+        WebElement element = PageUtil.getElement(driver, By.xpath("//button[@id='remove-sauce-labs-backpack']"));
+        boolean isDisplayed = element != null && element.isDisplayed();
+
+        PageUtil.subLog("Is the product removed on the inventory page ? " + isDisplayed);
+        Assert.assertFalse(isDisplayed, "Product is still displaying on the inventory page");
+    }
+
+
 
     @Test
     public void test1() throws InterruptedException {
@@ -131,6 +166,13 @@ public class Main {
 
         System.out.println("5. Go to 'Your Card' Page");
         goToYourCardPage(product);
+
+        System.out.println("6. Remove the product");
+        removeProduct2(product);
+
+        System.out.println("5. Check product removed in the inventory page");
+        goToInventoryPage();
+        verifyProductRemoved();
 
 
     }
